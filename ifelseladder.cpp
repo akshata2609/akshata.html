e1f307b6f1140315b377a688c1f8862f4ee88e6b
akshata2609 <akshu26091999@gmail.com>
2025-01-24 19:45:22 +0530

    second commit

---
+++ ifelseladder.cpp
@@ -0,0 +1,91 @@
+# include <iostream>
+using namespace std;
+int main(){
+     /*int a,b,c;
+     cout<<"enter number";
+     cin>>a>>b>>c;
+
+     if(a>b && b>c){
+        cout<<a;
+     }
+
+     else if(b>a && b>c){
+        cout<<b;
+     }
+
+     else{
+        cout<<c;
+     }
+
+   
+    int percent;
+    cout<<"enter percent";
+    cin>>percent;
+
+    if(percent<100 && percent>=50){
+        cout<<"A";
+    }
+
+    else if(percent<50 && percent>30){
+        cout<<"B";
+    }
+
+    else{
+        cout<<"FAIL";
+    }
+     
+
+    int percent;
+    cout<<"enter percent";
+    cin>>percent;
+
+    if(percent<=100 && percent>=80){
+        cout<<"A";
+    }
+
+    else if(percent<80 && percent>=60){
+        cout<<"B";
+    }
+
+    else if(percent<60 && percent>=30){
+        cout<<"C";
+    }
+
+    else if(percent<30 && percent>=0){
+        cout<<"FAIL";
+    } 
+
+    else{
+        cout<<"INVALID";
+    }
+    
+    int num;
+    cout<<"enter any number";
+    cin>>num;
+
+    if(num>0){
+        cout<<"POSITIVE";
+    }
+
+    else if(num<0){
+        cout<<"NEGATIVE";
+    }
+
+    else{
+        cout<<"ZERO";
+    }
+    
+    char check;
+    cout<<"enter any character";
+    cin>>check;
+
+    if(check=='a'|| check=='e' || check=='i' || check=='o'|| check=='u'){
+        cout<<"VOVEL";
+     }
+
+    else{
+        cout<<"CONSTRANTS";
+    } 
+    
+
+}
